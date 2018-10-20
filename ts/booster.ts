c16ba50835ca41029f79828cd72ef99b5030c1b7
Rachelle de Zwart <Rachelle207@live.nl>
2018-10-20 20:21:37 +0200

    added a test file for documentation

--- ts/booster.ts
+++ ts/booster.ts
@@ -3,4 +3,12 @@ class booster extends GameItem{
     constructor(radius:number, colour: string, xPosition: number = 0, yPosition: number = 0) {
         super(radius, colour, xPosition, yPosition);
     }
+    
+    /**
+    * Function to draw the initial state of the gameItem
+    * @param {HTMLElement} - container
+    */
+    public draw(): void {
+        
+    }
 }
